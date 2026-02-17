41f68292102a4923b1076c71de2ce955ec0f1a38
JCMLaptop <johnclyde.montana@gmail.com>
2026-02-17 18:00:15 +0100

    Iniciar main

---
+++ main.js
@@ -0,0 +1,19 @@
+// main.js
+// Programa principal de la calculadora
+
+const suma = require("./suma");
+const resta = require("./resta");
+const multiplicacion = require("./multiplicacion");
+const division = require("./division");
+
+let a = 10;
+let b = 5;
+
+console.log("Número A:", a);
+console.log("Número B:", b);
+console.log("------------------");
+
+console.log("Suma:", suma(a, b));
+console.log("Resta:", resta(a, b));
+console.log("Multiplicación:", multiplicacion(a, b));
+console.log("División:", division(a, b));
