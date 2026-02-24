745f681af4558daf54841e64b6e7cbe2e2ef64a1
daniellopezmarotogomez-glitch <daniellopezmarotogomez@gmail.com>
2026-02-24 17:47:11 +0100

    Resolviendo conflictos

--- operaciones.js
+++ operaciones.js
@@ -1,8 +1,3 @@
-function division(a, b) {
-    if (b === 0) {
-        return "Error: No es posible dividir entre cero";
-    }
-    return a / b;
+function restar(a, b) {
+    return a - b;
 }
-
-module.exports = division;
